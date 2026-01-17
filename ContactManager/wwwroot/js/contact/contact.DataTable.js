66794cc808e280b3d5908654aa4e3db28b5f207a
51dollar <borushkoddima@mail.ru>
2026-01-17 18:23:53 +0300

    add base UI

---
+++ ContactManager/wwwroot/js/contact/contact.DataTable.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    $('#contactsTable').DataTable({
+        paging: true,
+        searching: true,
+        ordering: true,
+        info: true,
+        lengthChange: false,
+        pageLength: 10,
+        language: {
+            search: "Search:",
+            info: "Showing _START_ to _END_ of _TOTAL_ contacts",
+            infoEmpty: "No contacts",
+            zeroRecords: "Nothing found"
+        },
+        columnDefs: [
+            { orderable: false, targets: 4 }
+        ]
+    });
+
+});
