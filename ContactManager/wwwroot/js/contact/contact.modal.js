d698179e4fb52222da33cccee49506e041c6a9cc
51dollar <borushkoddima@mail.ru>
2026-01-18 17:30:35 +0300

    fix validation

--- ContactManager/wwwroot/js/contact/contact.modal.js
+++ ContactManager/wwwroot/js/contact/contact.modal.js
@@ -1,36 +1,66 @@
-﻿$(document).ready(function () {
+﻿$(() => {
+    const $form = $('#contactForm');
+    const $modal = $('#contactModal');
+    const $modalTitle = $('#modalTitle');
+    const $addBtn = $('#addContactBtn');
 
-    const form = $('#contactForm');
+    const $id = $('input[name="Id"]');
+    const $name = $('#Name');
+    const $phone = $('#MobilePhone');
+    const $job = $('#JobTitle');
+    const $birth = $('#BirthDate');
 
-    function clearForm() {
-        form[0].reset();
-        $('input[name="Id"]').val('');
-        form.validate().resetForm();
-        form.find('.is-valid, .is-invalid')
+    let lastFocusedElement = null;
+
+    const clearFormState = () => {
+        $form[0]?.reset();
+
+        const validator = $form.data('validator');
+        validator?.resetForm();
+
+        $form.find('.is-valid, .is-invalid')
             .removeClass('is-valid is-invalid');
-    }
-    
-    $('#addContactBtn').on('click', function () {
-        clearForm();
-        $('#modalTitle').text('Create contact');
-        form.attr('action', '/Contact/Create');
+
+        $form.find('[aria-invalid]')
+            .attr('aria-invalid', 'false');
+
+        $form.find('[data-valmsg-for]')
+            .text('');
+
+        $form.find(':input').each((_, el) => {
+            el.setCustomValidity?.('');
+        });
+    };
+
+    $(document).on('click', '[data-bs-target="#contactModal"]', e => {
+        lastFocusedElement = e.currentTarget;
     });
-    
+
+    $addBtn.on('click', () => {
+        clearFormState();
+        $modalTitle.text('Create contact');
+        $form.attr('action', '/Contact/Create');
+    });
+
     $(document).on('click', '.edit-btn', function () {
-        clearForm();
+        clearFormState();
 
-        $('#modalTitle').text('Edit contact');
-        form.attr('action', '/Contact/Edit');
+        $modalTitle.text('Edit contact');
+        $form.attr('action', '/Contact/Edit');
 
-        $('input[name="Id"]').val($(this).data('id'));
-        $('#Name').val($(this).data('name'));
-        $('#MobilePhone').val($(this).data('phone'));
-        $('#JobTitle').val($(this).data('job'));
-        $('#BirthDate').val($(this).data('birth'));
+        $id.val($(this).data('id'));
+        $name.val($(this).data('name'));
+        $phone.val($(this).data('phone'));
+        $job.val($(this).data('job'));
+        $birth.val($(this).data('birth'));
     });
-    
-    $('#contactModal').on('hidden.bs.modal', function () {
-        clearForm();
-        $('#addContactBtn').trigger('focus');
+
+    $modal.on('hide.bs.modal', () => {
+        clearFormState();
+        $(lastFocusedElement || $addBtn).trigger('focus');
+    });
+
+    $modal.on('hidden.bs.modal', () => {
+        lastFocusedElement = null;
     });
 });
